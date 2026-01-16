68af7d0f89b3a4eea2f4088a1fdcc43f9aa82c82
Joel Erreyes <erreyesjoel.gms@gmail.com>
2026-01-16 16:54:06 +0100

    import os

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+
 # Configuració de Producció 
 MAX_RETRIES = 3 
-ADMIN_TOKEN = "12345" # <--- VULNERABLE 
+
+# Ara el token es llegeix de forma segura des de l'entorn
+ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")
